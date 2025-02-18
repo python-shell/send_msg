cbd8240ec678352c07a1438456418747b497f742
python-shell <778424955@qq.com>
2025-02-18 23:02:41 +0800

    Update gemini.py

--- gemini.py
+++ gemini.py
@@ -1,5 +1,7 @@
 import google.generativeai as genai
 
+from sendmail import sendmail
+
 # 设置 API Key
 genai.configure(api_key="AIzaSyDh7LYrvPiqUf30NKVGN4v1HRqBRbfOpRQ")
 
@@ -9,5 +11,5 @@ model = genai.GenerativeModel("gemini-2.0-flash")
 # 发送请求并获取响应
 response = model.generate_content(contents="请帮我搜索一下当前的对股票有影响的实事新闻并总结给我")
 
-# 输出结果
-print(response.text)
+# 邮件发送
+sendmail(response.text)
